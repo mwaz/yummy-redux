b548ab67a3f62c71f95a90b035ec495f2c3e5cad
mwaz <amby.mwaz@gmail.com>
2018-04-05 00:36:45 +0300

    [fx] add error handling in recipe add modal

--- src/components/categories/recipes/forms/AddRecipe.js
+++ src/components/categories/recipes/forms/AddRecipe.js
@@ -1,68 +1,77 @@
 import React from 'react';
-import { Modal, Row, Button, Input } from 'react-materialize';
+import { Modal, Row, Button, Input, Col } from 'react-materialize';
+import InlineError from '../../../messages/InlineError';
 
 class AddRecipe extends React.Component {
   render() {
+    const errors = this.props.errors;
+    console.log(errors);
     return (
       <div>
-        <Modal
-          id={this.props.recipe_id}
-          //   open={this.state.isOpen}
-          header="Add New Recipe"
-          trigger={
-            <Button
-              style={{ marginRight: '30px' }}
-              className="btn-floating btn-large waves-effect waves-light red"
-              onSubmit={this.props.onSubmit}
+        <Row>
+          <Col s={9}>
+            <Modal
+              id={this.props.recipe_id}
+              //   open={this.state.isOpen}
+              header="Add New Recipe"
+              trigger={
+                <Button
+                  style={{ marginRight: '30px' }}
+                  className="btn-floating btn-large waves-effect waves-light red"
+                >
+                  <i className="material-icons">add</i>
+                </Button>
+              }
             >
-              <i className="material-icons">add</i>
-            </Button>
-          }
-        >
-          <form onSubmit={this.props.onSubmit}>
-            <div>
-              <Row>
-                <Input
-                  s={6}
-                  label="recipe Name"
-                  name="recipe_name"
-                  validate
-                  value={this.props.category_name}
-                  onChange={this.onChange}
-                />
-              </Row>
-              <Row>
-                <Input
-                  s={6}
-                  label="Recipe Ingredients"
-                  name="recipe_ingredients"
-                  validate
-                  value={this.props.recipe_ingredients}
-                  onChange={this.onChange}
-                />
-              </Row>
-              <Row>
-                <Input
-                  s={6}
-                  label="Recipe Methods"
-                  name="recipe_ingredients"
-                  validate
-                  value={this.props.recipe_methods}
-                  onChange={this.onChange}
-                />
-              </Row>
-              <Button
-                type="submit"
-                waves="light"
-                onSubmit={this.props.onSubmit}
-              >
-                Add Recipe
-              </Button>
+              <form onSubmit={this.props.onSubmit}>
+                <div>
+                  <Row>
+                    {errors && <InlineError text={errors.recipe_name} />}
+                    <Input
+                      s={8}
+                      label="recipe Name"
+                      name="recipe_name"
+                      validate
+                      value={this.props.category_name}
+                      onChange={this.onChange}
+                    />
+                  </Row>
+                  <Row>
+                    {errors && <InlineError text={errors.recipe_ingredients} />}
+                    <Input
+                      s={8}
+                      label="Recipe Ingredients"
+                      name="recipe_ingredients"
+                      validate
+                      value={this.props.recipe_ingredients}
+                      onChange={this.onChange}
+                    />
+                  </Row>
+                  <Row>
+                    {errors && <InlineError text={errors.recipe_methods} />}
+                    <Input
+                      s={8}
+                      label="Recipe Methods"
+                      name="recipe_ingredients"
+                      validate
+                      value={this.props.recipe_methods}
+                      onChange={this.onChange}
+                    />
+                  </Row>
+                  <Button
+                    type="submit"
+                    waves="light"
+                    onSubmit={this.props.onSubmit}
+                  >
+                    Add Recipe
+                  </Button>
 
-              <Row />
-            </div>
-          </form>
-        </Modal>
+                  <Row />
+                </div>
+              </form>
+            </Modal>
+          </Col>
+        </Row>
       </div>
     );
   }
