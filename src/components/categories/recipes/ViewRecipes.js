60a8307c78e777736aefefc49e4c9f41626c5207
mwaz <amby.mwaz@gmail.com>
2018-04-04 23:57:34 +0300

    [ft] create new component for holding recipes

---
+++ src/components/categories/recipes/ViewRecipes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import RecipeComponent from './Recipes';
+
+class ViewRecipes extends React.Component {
+  render() {
+    return (
+      <div>
+        <RecipeComponent />
+      </div>
+    );
+  }
+}
+export default ViewRecipes;
